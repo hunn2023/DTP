d50c506184da179d520c176f4dfa2f4a7001661c
hunn2023 <admin@DESKTOP-8490O21>
2026-06-13 00:48:39 +0700

    fix:update migration

--- BE/DTP/src/Modules/DTP.Modules.Auth/Migrations/20260612174821_AddProductImages.Designer.cs
+++ BE/DTP/src/Modules/DTP.Modules.Auth/Migrations/20260612174821_AddProductImages.Designer.cs
@@ -4,6 +4,7 @@ using DTP.Modules.Auth.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DTP.Modules.Auth.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260612174821_AddProductImages")]
+    partial class AddProductImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BE/DTP/src/Modules/DTP.Modules.Auth/Migrations/20260612174821_AddProductImages.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTP.Modules.Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetOtpExpiredAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetOtpHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PasswordResetVerifyFailedCount",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetOtpExpiredAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetOtpHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetVerifyFailedCount",
+                table: "Users");
+        }
+    }
+}
