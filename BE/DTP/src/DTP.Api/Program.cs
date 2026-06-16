220f467b635e970102d2b57c536a30b5459e8c2d
hunn2023 <admin@DESKTOP-8490O21>
2026-06-16 15:20:08 +0700

    .

--- BE/DTP/src/DTP.Api/Program.cs
+++ BE/DTP/src/DTP.Api/Program.cs
@@ -33,7 +33,8 @@ namespace DTP.Api
                             "https://dtp-admin-c5dd3.web.app",
                             "https://dtpweb-94f64.web.app",
                             "http://localhost:5173",
-                            "http://localhost:3000"
+                            "http://localhost:3000",
+                            "https://ezsim.vercel.app"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod()
