399690711c659bcdc42604c251bbc76eafeec7b1
hunn2023 <admin@DESKTOP-8490O21>
2026-06-29 00:58:19 +0700

    .

---
+++ BE/DTP/src/Modules/DTP.Modules.Payment/Migrations/20260628175811_UpdateTable2906.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTP.Modules.Payment.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTable2906 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "PaymentTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentProviderCode",
+                table: "PaymentTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentProviderCode",
+                table: "PaymentTransactions");
+        }
+    }
+}
